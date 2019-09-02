b15d8020a9fdcf8a5240ba427c2b65cec167dd94
andrey.radzkov <Andrey.Radzkov@greensky.com>
2019-09-02 15:59:27 +0300

    distance to point

--- src/SwitchExample.js
+++ src/SwitchExample.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import { View, Text, Switch, StyleSheet, PermissionsAndroid,AsyncStorage ,} from 'react-native'
 import Geolocation from '@react-native-community/geolocation';
+import getDistance from 'geolib/es/getDistance';
 
 export class SwitchExample extends Component {
    state = {
       initialPosition: 'unknown',
       lastPosition: 'unknown',
+      distance: 'unknown',
+      latitude: 'unknown',
+      longitude: 'unknown',
    }
    watchID = null;
    componentDidMount = () => {
@@ -22,15 +26,26 @@ export class SwitchExample extends Component {
                    const initialPosition = JSON.stringify(position);
                    this.setState({ initialPosition });
                 },
-                (error) => alert(error.message),
-                { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 , distanceFilter: 5}
+                (error) => alert("initial: " + error.message),
+                { enableHighAccuracy: true, timeout: 30000, maximumAge: 1000 , distanceFilter: 5}
              );
              this.watchID = Geolocation.watchPosition((position) => {
                 const lastPosition = JSON.stringify(position);
-                this.setState({ lastPosition });
+                if(position && position.coords ){
+                    var distance =  getDistance(
+                                        { latitude: position.coords.latitude, longitude: position.coords.longitude },
+                                         //{ latitude: 53.8841564, longitude: 27.4491562 },
+                                          { latitude: 53.91783263, longitude: 27.59183951 },
+                                    );
+                    if(distance<120){
+    //                alert("we are at home");
+                        alert("we are at work"); // TODO: remember about accuracy
+                    }
+                    this.setState({ lastPosition ,distance,latitude: position.coords.latitude,longitude: position.coords.longitude});
+                }
              },
-                       (error) => alert(error.message),
-                       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 , distanceFilter: 5}
+                       (error) => alert("current: " + error.message),
+                       { enableHighAccuracy: true, timeout: 30000, maximumAge: 1000 , distanceFilter: 5}
              );
              }
        });
@@ -57,6 +72,24 @@ export class SwitchExample extends Component {
             <Text>
                {this.state.lastPosition}
             </Text>
+            <Text style = {styles.boldText}>
+               Distance:
+            </Text>
+            <Text>
+               {this.state.distance}
+            </Text>
+             <Text style = {styles.boldText}>
+               latitude:
+            </Text>
+            <Text>
+               {this.state.latitude}
+            </Text>
+              <Text style = {styles.boldText}>
+                 longitude:
+              </Text>
+              <Text>
+                  {this.state.longitude}
+               </Text>
          </View>
       )
    }
