1de271c56c2db3d7207ef15821d5f82fcba7a4d6
liniyakamnya <liniyakamnya@gmail.com>
2019-09-06 21:20:45 +0300

    compilation fix

--- src/SwitchExample.js
+++ src/SwitchExample.js
@@ -1,135 +1,134 @@
-import React, { Component } from 'react'
-import { View, Text, Switch, StyleSheet, PermissionsAndroid,AsyncStorage ,Linking,Button ,} from 'react-native'
-import Geolocation from '@react-native-community/geolocation';
-import getDistance from 'geolib/es/getDistance';
-import RNImmediatePhoneCall from 'react-native-immediate-phone-call';
-import Contacts from 'react-native-contacts';
-import { gyroscope, setUpdateIntervalForType ,SensorTypes  } from "react-native-sensors";
-import { map, filter } from "rxjs/operators";
-import { startDetector, stopDetector } from "./parkingDetector";
+import React, {Component} from "react";
+import {Button, PermissionsAndroid, StyleSheet, Text, View} from "react-native";
+import RNImmediatePhoneCall from "react-native-immediate-phone-call";
+import Contacts from "react-native-contacts";
+import {gyroscope, SensorTypes, setUpdateIntervalForType} from "react-native-sensors";
+import {startDetector, stopDetector} from "./parkingDetector";
 
 export class SwitchExample extends Component {
-   state = {
-      lastPosition: 'unknown',
-      distance: 'unknown',
-      latitude: 'unknown',
-      longitude: 'unknown',
-      accuracy: 'unknown',
-      parking: 'unknown',
-      gyroscope: 'unknown',
-      angleY: 0.0,
-      stepLabel: "Not started",
-   }
+    state = {
+        lastPosition: 'unknown',
+        distance: 'unknown',
+        latitude: 'unknown',
+        longitude: 'unknown',
+        accuracy: 'unknown',
+        parking: 'unknown',
+        gyroscope: 'unknown',
+        angleY: 0.0,
+        stepLabel: "Not started",
+    }
 
-   componentDidMount = () => {
-        startDetector( this);
-   }
-   componentWillUnmount = () => {
+    componentDidMount = () => {
+        startDetector(this);
+    }
+    componentWillUnmount = () => {
         stopDetector();
-   }
-   call=()=>{
-     PermissionsAndroid.request(
+    }
+    call = () => {
+        PermissionsAndroid.request(
             PermissionsAndroid.PERMISSIONS.CALL_PHONE,
             {
-              title: "Auto call permission",
-              message: "App needs access to call"
+                title: "Auto call permission",
+                message: "App needs access to call"
             }
-          ).then((granted)=>{
+        ).then((granted) => {
             if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-           PermissionsAndroid.request(
-             PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
-             {
-               'title': 'Contacts',
-               'message': 'This app would like to view your contacts.'
-             }
-           ).then(() => {
-             Contacts.getAll((err, contacts) => {
-               if (err === 'denied'){
-                 // error
-               } else {
+                PermissionsAndroid.request(
+                    PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+                    {
+                        'title': 'Contacts',
+                        'message': 'This app would like to view your contacts.'
+                    }
+                ).then(() => {
+                    Contacts.getAll((err, contacts) => {
+                        if (err === 'denied') {
+                            // error
+                        } else {
 //               var parking = contacts.filter(contact=>contact.displayName==='Парковка');
-               var parking = contacts.filter(contact=>contact.displayName==='Жена');
-               var parkingNumber = parking[0].phoneNumbers[0].number;
-                  this.setState({parking:parkingNumber});
-                RNImmediatePhoneCall.immediatePhoneCall(parkingNumber);
+                            var parking = contacts.filter(contact => contact.displayName === 'Жена');
+                            var parkingNumber = parking[0].phoneNumbers[0].number;
+                            this.setState({parking: parkingNumber});
+                            RNImmediatePhoneCall.immediatePhoneCall(parkingNumber);
 
-               }
-             })
-           })
-                }});
-   }
-   render() {
-      return (
-         <View style = {styles.container}>
+                        }
+                    })
+                })
+            }
+        });
+    }
 
-            <Text style = {styles.boldText}>
-               Step:
-            </Text>
-            <Text>
-               {this.state.stepLabel}
-            </Text>
-            <Text style = {styles.boldText}>
-               Distance:
-            </Text>
-            <Text>
-               {this.state.distance}
-            </Text>
-             <Text style = {styles.boldText}>
-               latitude:
-            </Text>
-            <Text>
-               {this.state.latitude}
-            </Text>
-              <Text style = {styles.boldText}>
-                 longitude:
-              </Text>
-              <Text>
-                  {this.state.longitude}
-               </Text>
-                <Text style = {styles.boldText}>
-                 accuracy:
-              </Text>
-              <Text>
-                  {this.state.accuracy}
-               </Text>
-               <Button
-                title="Call"
-                accessibilityLabel="Learn more about this purple button"
-                onPress={this.call}/>
-                 <Text style = {styles.boldText}>
-                                 parking:
-                              </Text>
-                              <Text>
-                                  {this.state.parking}
-                               </Text>
-                               <Text style = {styles.boldText}>
-                                 gyroscope:
-                              </Text>
-                              <Text>
-                                  {this.state.gyroscope}
-                               </Text>
-                                <Text style = {styles.boldText}>
-                                 Angle Y:
-                              </Text>
-                              <Text>
-                                  {parseFloat(this.state.angleY).toFixed(3)}
-                               </Text>
+    render() {
+        return (
+            <View style={styles.container}>
+
+                <Text style={styles.boldText}>
+                    Step:
+                </Text>
+                <Text>
+                    {this.state.stepLabel}
+                </Text>
+                <Text style={styles.boldText}>
+                    Distance:
+                </Text>
+                <Text>
+                    {this.state.distance}
+                </Text>
+                <Text style={styles.boldText}>
+                    latitude:
+                </Text>
+                <Text>
+                    {this.state.latitude}
+                </Text>
+                <Text style={styles.boldText}>
+                    longitude:
+                </Text>
+                <Text>
+                    {this.state.longitude}
+                </Text>
+                <Text style={styles.boldText}>
+                    accuracy:
+                </Text>
+                <Text>
+                    {this.state.accuracy}
+                </Text>
+                <Button
+                    title="Call"
+                    accessibilityLabel="Learn more about this purple button"
+                    onPress={this.call}/>
+                <Text style={styles.boldText}>
+                    parking:
+                </Text>
+                <Text>
+                    {this.state.parking}
+                </Text>
+                <Text style={styles.boldText}>
+                    gyroscope:
+                </Text>
+                <Text>
+                    {this.state.gyroscope}
+                </Text>
+                <Text style={styles.boldText}>
+                    Angle Y:
+                </Text>
+                <Text>
+                    {parseFloat(this.state.angleY).toFixed(3)}
+                </Text>
 
 
-         </View>
-      )
-   }
+            </View>
+        )
+    }
 }
 
-const styles = StyleSheet.create ({
-   container: {
-      flex: 1,
-      alignItems: 'center',
-      marginTop: 50,
-      marginBottom: 50,
-   },
-   boldText: {
-      fontSize: 30,
-      color: 'red',
-   }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        marginTop: 50,
+        marginBottom: 50,
+    },
+    boldText: {
+        fontSize: 30,
+        color: 'red',
+    }
 })
--- src/parkingDetector.js
+++ src/parkingDetector.js
@@ -1,128 +1,146 @@
-import React, { Component } from 'react'
-import { View, Text, Switch, StyleSheet, PermissionsAndroid,AsyncStorage ,Linking,Button ,} from 'react-native'
-import Geolocation from '@react-native-community/geolocation';
-import getDistance from 'geolib/es/getDistance';
-import RNImmediatePhoneCall from 'react-native-immediate-phone-call';
-import Contacts from 'react-native-contacts';
-import { gyroscope, setUpdateIntervalForType ,SensorTypes  } from "react-native-sensors";
-import { map, filter } from "rxjs/operators";
+import React, {Component} from "react";
+import {PermissionsAndroid} from "react-native";
+import Geolocation from "@react-native-community/geolocation";
+import getDistance from "geolib/es/getDistance";
+import RNImmediatePhoneCall from "react-native-immediate-phone-call";
+import Contacts from "react-native-contacts";
+import {gyroscope, SensorTypes, setUpdateIntervalForType} from "react-native-sensors";
 const refreshInterval = 80;
 setUpdateIntervalForType(SensorTypes.gyroscope, refreshInterval);
 
- var steps=[
- {executed:false, minSecToNext: 2,maxSecToNext: 120,   coordinate:{ latitude: 53.917537065078584, longitude: 27.590843470480717 }, label: "One"},
- {executed:false,  minSecToNext: 2, maxSecToNext: 120, coordinate:{ latitude: 53.91831029348834, longitude: 27.589849082844445 }, label: "Two"},
- {executed:false, angleY: 90.0, label: "Three"}]
+var steps = [
+    {
+        executed: false,
+        minSecToNext: 2,
+        maxSecToNext: 120,
+        coordinate: {latitude: 53.917537065078584, longitude: 27.590843470480717},
+        label: "One"
+    },
+    {
+        executed: false,
+        minSecToNext: 2,
+        maxSecToNext: 120,
+        coordinate: {latitude: 53.91831029348834, longitude: 27.589849082844445},
+        label: "Two"
+    },
+    {executed: false, angleY: 90.0, label: "Three"}]
 
- var startTime = null;
- var watchID = null;
- var grad = 57.2957795131;
+var startTime = null;
+var watchID = null;
+var grad = 57.2957795131;
 export const startDetector = (component) => {
- return PermissionsAndroid.request(
-         PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-         {
-           title: "Location Accessing Permission",
-           message: "App needs access to your location"
-         }
-       ).then((granted)=>{
+    return PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+            title: "Location Accessing Permission",
+            message: "App needs access to your location"
+        }
+    ).then((granted) => {
 //        alert("results: " + granted);
-         if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
             //don`t know why we need this but we need...
             Geolocation.getCurrentPosition(
                 (position) => {
-                   component.setState({ latitude: position.coords.latitude,longitude: position.coords.longitude });
+                    component.setState({latitude: position.coords.latitude, longitude: position.coords.longitude});
                 },
                 (error) => alert("initial: " + error.message),
-                { enableHighAccuracy: true, timeout: 30000, maximumAge: 1000 , distanceFilter: 5}
-             );
-             watchID = Geolocation.watchPosition((position) => {
-                const lastPosition = JSON.stringify(position);
+                {enableHighAccuracy: true, timeout: 30000, maximumAge: 1000, distanceFilter: 5}
+            );
+            watchID = Geolocation.watchPosition((position) => {
+                    const lastPosition = JSON.stringify(position);
 //                alert("we are in " + lastPosition);
-                if(position && position.coords ){
-                    var step = steps.filter(step=>!step.executed );
-                    var distance = 0;
-                    var stepLabel = null
-                    if(step[0]){
-                        if(step[0].coordinate){
-                            distance =  getDistance(
-                                              { latitude: position.coords.latitude, longitude: position.coords.longitude },
-                                               step[0].coordinate);
-                                            if(distance<Math.max(Math.min(40, position.coords.accuracy), 15)){ //tDOO check time
-                                            step[0].executed = true;
-                                            stepLabel = step[0].label;
-                            //                        this.call() // TODO: remember about accuracy
-                                            }
-                         } else if(step[0].angleY){
+                    if (position && position.coords) {
+                        var step = steps.filter(step => !step.executed);
+                        var distance = 0;
+                        var stepLabel = null
+                        if (step[0]) {
+                            if (step[0].coordinate) {
+                                distance = getDistance(
+                                    {latitude: position.coords.latitude, longitude: position.coords.longitude},
+                                    step[0].coordinate);
+                                if (distance < Math.max(Math.min(40, position.coords.accuracy), 15)) { //tDOO check time
+                                    step[0].executed = true;
+                                    stepLabel = step[0].label;
+                                    //                        this.call() // TODO: remember about accuracy
+                                }
+                            } else if (step[0].angleY) {
 
-                                gyroscope.subscribe(({ x, y, z, timestamp  }) => {
-                             //TODO: we should use y axel
+                                gyroscope.subscribe(({x, y, z, timestamp}) => {
+                                    //TODO: we should use y axel
                                     var angleY = component.state.angleY;
-                                    if(angleY>step[0].angleY*0.85){
+                                    if (angleY > step[0].angleY * 0.85) {
                                         step[0].executed = true;
-                                        stepLabel=step[0].label;
+                                        stepLabel = step[0].label;
 
-                                        setInterval(()=>{
+                                        setInterval(() => {
 
                                             PermissionsAndroid.request(
-                                                    PermissionsAndroid.PERMISSIONS.CALL_PHONE,
-                                                    {
-                                                      title: "Auto call permission",
-                                                      message: "App needs access to call"
-                                                    }
-                                                  ).then((granted)=>{
-                                                    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-                                                   PermissionsAndroid.request(
-                                                     PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
-                                                     {
-                                                       'title': 'Contacts',
-                                                       'message': 'This app would like to view your contacts.'
-                                                     }
-                                                   ).then(() => {
-                                                     Contacts.getAll((err, contacts) => {
-                                                       if (err === 'denied'){
-                                                         // error
-                                                       } else {
-                                        //               var parking = contacts.filter(contact=>contact.displayName==='Парковка');
-                                                       var parking = contacts.filter(contact=>contact.displayName==='Жена');
-                                                       var parkingNumber = parking[0].phoneNumbers[0].number;
-                                                          this.setState({parking:parkingNumber});
-                                                        RNImmediatePhoneCall.immediatePhoneCall(parkingNumber);
+                                                PermissionsAndroid.PERMISSIONS.CALL_PHONE,
+                                                {
+                                                    title: "Auto call permission",
+                                                    message: "App needs access to call"
+                                                }
+                                            ).then((granted) => {
+                                                if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                                                    PermissionsAndroid.request(
+                                                        PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+                                                        {
+                                                            'title': 'Contacts',
+                                                            'message': 'This app would like to view your contacts.'
+                                                        }
+                                                    ).then(() => {
+                                                        Contacts.getAll((err, contacts) => {
+                                                            if (err === 'denied') {
+                                                                // error
+                                                            } else {
+                                                                //               var parking = contacts.filter(contact=>contact.displayName==='Парковка');
+                                                                var parking = contacts.filter(contact => contact.displayName === 'Жена');
+                                                                var parkingNumber = parking[0].phoneNumbers[0].number;
+                                                                component.setState({parking: parkingNumber});
+                                                                RNImmediatePhoneCall.immediatePhoneCall(parkingNumber);
 
-                                                       }
-                                                     })
-                                                   })
-                                                        }});
+                                                            }
+                                                        })
+                                                    })
+                                                }
+                                            });
 
-                                        },3000);
+                                        }, 3000);
 
 
                                     }
                                     component.setState({
                                         gyroscope: `y: ${(y * grad).toFixed(3)}`,
-                                        angleY: (angleY + (y * grad) * (1.0/(1000 / refreshInterval)))
+                                        angleY: (angleY + (y * grad) * (1.0 / (1000 / refreshInterval)))
                                     });
-                            });
-                         }
-                    }
+                                });
+                            }
+                        }
 
-                    var newState={ lastPosition ,distance,latitude: position.coords.latitude,longitude: position.coords.longitude, accuracy:`real: ${position.coords.accuracy}; my: ${Math.max(Math.min(40, position.coords.accuracy), 15)}`};
-                    if(stepLabel){
-                        newState.stepLabel = stepLabel;
+                        var newState = {
+                            lastPosition,
+                            distance,
+                            latitude: position.coords.latitude,
+                            longitude: position.coords.longitude,
+                            accuracy: `real: ${position.coords.accuracy}; my: ${Math.max(Math.min(40, position.coords.accuracy), 15)}`
+                        };
+                        if (stepLabel) {
+                            newState.stepLabel = stepLabel;
+                        }
+                        component.setState(newState);
                     }
-                    component.setState(newState);
-                }
-             },
-                       (error) => alert("current: " + error.message),
-                       { enableHighAccuracy: true, timeout: 30000, maximumAge: 1000 , distanceFilter: 5}
-             );
-             }
+                },
+                (error) => alert("current: " + error.message),
+                {enableHighAccuracy: true, timeout: 30000, maximumAge: 1000, distanceFilter: 5}
+            );
+        }
 
-       });
+    });
 
 
 }
 
 export const stopDetector = () => {
-      Geolocation.clearWatch(watchID);
+    Geolocation.clearWatch(watchID);
 
 }
