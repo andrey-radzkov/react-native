99752e5f89d8fecb5f217a501ca6387a13b1534a
andrey.radzkov <Andrey.Radzkov@greensky.com>
2020-03-05 18:32:30 +0300

    external steps

---
+++ src/steps.js
@@ -0,0 +1,17 @@
+export var steps = [
+  {
+    executed: false,
+    minSecToNext: 2,
+    maxSecToNext: 120,
+    coordinate: {latitude: 53.882943464064724, longitude: 27.503906629169332},
+    label: "One"
+  },
+  {
+    executed: false,
+    minSecToNext: 2,
+    maxSecToNext: 120,
+    coordinate: {latitude: 53.88251559426032, longitude: 27.502957127178075},
+    label: "Two"
+  },
+  {executed: false, angleY: 90.0, label: "Three"}
+];
