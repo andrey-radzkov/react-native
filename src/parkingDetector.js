fd226048d8a32291e36455184f976be39b59c81c
andrey.radzkov <Andrey.Radzkov@greensky.com>
2020-03-09 17:41:13 +0300

    vertical detection

--- src/parkingDetector.js
+++ src/parkingDetector.js
@@ -59,11 +59,16 @@ export const startDetector = () => wrap(async (dispatch, state) => {
   });
   magnetometer.subscribe(({x, y, z, timestamp}) => {
     let angle = 0;
-    if (Math.atan2(y, x) >= 0) {
-      angle = Math.atan2(y, x) * (180 / Math.PI)
+    if (Math.atan2(-z, x) >= 0) {
+      angle = Math.atan2(-z, x) * (180 / Math.PI)
     } else {
-      angle = (Math.atan2(y, x) + 2 * Math.PI) * (180 / Math.PI)
+      angle = (Math.atan2(-z, x) + 2 * Math.PI) * (180 / Math.PI)
     }
+    // if (Math.atan2(y, x) >= 0) {
+    //   angle = Math.atan2(y, x) * (180 / Math.PI)
+    // } else {
+    //   angle = (Math.atan2(y, x) + 2 * Math.PI) * (180 / Math.PI)
+    // }
     dispatch({
       type: "update", data: {
         magnetometer: `x: ${x.toFixed(1)} y: ${y.toFixed(1)} z: ${z.toFixed(1)}}`,
